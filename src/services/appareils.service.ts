f9f9d53037c86f2b509e780e39afcc9d02e82984
Hamdi MILED <hamdi.miled@sofia.local>
2018-11-05 10:02:11 +0100

    basic

---
+++ src/services/appareils.service.ts
@@ -0,0 +1,31 @@
+import { Appareil } from '../models/Appareil';
+
+export class AppareilsService {
+  appareilsList: Appareil[] = [
+    {
+      name: 'Machine à laver',
+      description: [
+        'Volume: 6 litres',
+        'Temps de lavage: 2 heures',
+        'Consommation: 173 kWh/an'
+      ],
+      isOn: true
+    },
+    {
+      name: 'Télévision',
+      description: [
+        'Dimensions: 40 pouces',
+        'Consommation: 22 kWh/an'
+      ],
+      isOn: true
+    },
+    {
+      name: 'Ordinateur',
+      description: [
+        'Marque: fait maison',
+        'Consommation: 500 kWh/an'
+      ],
+      isOn: false
+    }
+  ];
+}
